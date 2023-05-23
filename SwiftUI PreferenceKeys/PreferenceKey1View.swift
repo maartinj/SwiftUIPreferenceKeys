e8f0794269ab7d349da49c6e92d33e088da3c782
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-05-23 21:11:44 +0200

    feat: Example 1 complete

--- SwiftUI PreferenceKeys/PreferenceKey1View.swift
+++ SwiftUI PreferenceKeys/PreferenceKey1View.swift
@@ -8,6 +8,8 @@
 // Subscribe on YouTube: https://youTube.com/@StewartLynch
 // Buy me a ko-fi:  https://ko-fi.com/StewartLynch
 
+// Link: https://www.youtube.com/watch?v=GhIP98ht7Bk&ab_channel=StewartLynch
+
 
 import SwiftUI
 
@@ -22,8 +24,16 @@ struct PreferenceKey1View: View {
                     Rectangle()
                     Rectangle()
                 }
+                .updateString("Second")
                 Rectangle()
+                    .updateString("First")
             }
+            .navigationTitle("Preference Keys")
+        }
+        .updateString("Third")
+//        .preference(key: UpdateString.self, value: "First")
+        .onPreferenceChange(UpdateString.self) { newValue in
+            result = newValue
         }
     }
 }
@@ -31,5 +41,19 @@ struct PreferenceKey1View: View {
 struct PreferencesKey1View_Previews: PreviewProvider {
     static var previews: some View {
         PreferenceKey1View()
+    }
+}
+
+extension View {
+    func updateString(_ value: String) -> some View {
+        self.preference(key: UpdateString.self, value: value)
+    }
+}
+
+struct UpdateString: PreferenceKey {
+    static var defaultValue: String = ""
+    
+    static func reduce(value: inout String, nextValue: () -> String) {
+        value = nextValue()
     }
 }
