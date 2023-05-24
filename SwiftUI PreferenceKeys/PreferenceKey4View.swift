e7dd9f4768826976bd9da1c1336ab5a38892851c
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-05-24 04:17:08 +0200

    feat: Example 4 - With GeometryReader (completed SwiftUI PreferenceKeys)

--- SwiftUI PreferenceKeys/PreferenceKey4View.swift
+++ SwiftUI PreferenceKeys/PreferenceKey4View.swift
@@ -12,16 +12,26 @@
 import SwiftUI
 
 struct PreferenceKey4View: View {
-    var strings = ["Something", "Something Bigger", "Small"]
+    var strings = ["Something Really Really big", "Something Bigger", "Small"]
+    @State private var maxWidth: CGFloat = 300
     var body: some View {
         VStack {
             ForEach(strings, id: \.self) { string in
                 Text(string)
+                    .overlay {
+                        GeometryReader { proxy in
+                            Color.clear
+                                .maxViewWidth(proxy.size.width)
+                        }
+                    }
             }
             Rectangle()
                 .fill(Color.blue)
-                .frame(width: 300, height: 2)
+                .frame(width: maxWidth, height: 2)
                 .padding()
+        }
+        .onPreferenceChange(MaxViewWidth.self) { newValue in
+            maxWidth = newValue
         }
      }
 }
@@ -29,5 +39,19 @@ struct PreferenceKey4View: View {
 struct PreferenceKey4View_Previews: PreviewProvider {
     static var previews: some View {
         PreferenceKey4View()
+    }
+}
+
+struct MaxViewWidth: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = max(value, nextValue())
+    }
+}
+
+extension View {
+    func maxViewWidth(_ value: CGFloat) -> some View {
+        self.preference(key: MaxViewWidth.self, value: value)
     }
 }
